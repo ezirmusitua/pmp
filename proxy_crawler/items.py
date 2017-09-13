3a16c557cdf82c109c8bcdb0ff8efc34a817fdf3
ezirmusitua <jferroal@gmail.com>
2017-09-13 10:37:43 +0800

    Enable crawlera middleware
    Took 14 minutes

--- proxy_crawler/items.py
+++ proxy_crawler/items.py
@@ -5,7 +5,11 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
+import time
+import datetime
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, MapCompose
 
 
 class Proxy(scrapy.Item):
@@ -20,3 +24,40 @@ class Proxy(scrapy.Item):
     last_check_at = scrapy.Field()
     anonymity = scrapy.Field()
     pass
+
+
+def strip(in_str):
+    return in_str.strip()
+
+
+def to_int(in_str):
+    return int(in_str)
+
+
+def default_http(in_str):
+    return in_str.strip() if in_str is not None else 'HTTP'
+
+
+def speed_to_quality(in_str):
+    return in_str[:-1]
+
+
+def time_str_to_stamp(in_str):
+    # Example: 2017-09-12 14:26:59
+    return time.mktime(datetime.datetime.strptime(in_str, "%Y-%m-%d %H:%M:%S").timetuple())
+
+
+def default_anonymity(in_str):
+    return in_str.strip() if in_str else 'unknown'
+
+
+class CNProxyItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
+    ip_address_in = MapCompose(TakeFirst, strip)
+    port_in = MapCompose(TakeFirst, to_int)
+    type_in = MapCompose(TakeFirst,  default_http)
+    location_in = MapCompose(TakeFirst, strip)
+    quality_in = MapCompose(TakeFirst, speed_to_quality)
+    last_check_at = MapCompose(TakeFirst, time_str_to_stamp)
+    anonymity = MapCompose(TakeFirst, default_anonymity)
--- proxy_crawler/spiders/cnproxy.py
+++ proxy_crawler/spiders/cnproxy.py
@@ -1,21 +1,10 @@
-# -*- coding: utf-8 -*-
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
 
 
-class CnproxySpider(CrawlSpider):
-    name = 'cnproxy'
-    allowed_domains = ['cn-proxy.com']
-    start_urls = ['http://cn-proxy.com/']
+class CnproxySpider(scrapy.Spider):
+    name = "cnproxy"
+    start_urls = ['http://cn-proxy.com']
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
-
-    def parse_item(self, response):
-        i = {}
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+    def parse(self, response):
+        with open('cnproxy.html', 'wb') as f:
+            f.write(response.body)
