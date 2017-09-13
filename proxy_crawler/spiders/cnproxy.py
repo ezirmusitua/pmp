537d57a4872de9b6a71e50d1d4aeddd1db742355
ezirmusitua <jferroal@gmail.com>
2017-09-13 11:16:11 +0800

    Update cnproxy crawler
    Took 44 minutes

--- proxy_crawler/spiders/cnproxy.py
+++ proxy_crawler/spiders/cnproxy.py
@@ -1,10 +1,25 @@
 import scrapy
 
+from proxy_crawler.items import Proxy, CNProxyItemLoader
+
 
 class CnproxySpider(scrapy.Spider):
     name = "cnproxy"
     start_urls = ['http://cn-proxy.com']
 
     def parse(self, response):
-        with open('cnproxy.html', 'wb') as f:
-            f.write(response.body)
+        self.logger.info('Crawling cn proxy. ')
+        proxies = []
+        rows = response.css('.sortable > tbody > tr')
+        for row in rows:
+            loader = CNProxyItemLoader(item=Proxy(), selector=row)
+            loader.add_css('ip_address', 'td:nth-child(1)::text')
+            loader.add_value('type', 'HTTP')
+            loader.add_css('port', 'td:nth-child(2)::text')
+            loader.add_css('location', 'td:nth-child(3)::text')
+            loader.add_value('anonymity', 'unknown')
+            loader.add_value('quality', 'unknown')
+            loader.add_css('last_check_at', 'td:nth-child(5)::text')
+            proxies.append(loader.load_item())
+        self.logger.info('total count: {}'.format(len(proxies)))
+        return proxies
