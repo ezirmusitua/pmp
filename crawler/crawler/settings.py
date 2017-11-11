7cdb3e22c89d7bd264fdb9044cd8bebefa879510
ezirmusitua <jferroal@gmail.com>
2017-11-12 01:12:28 +0800

    Update settings
    Took 4 hours 38 minutes

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -75,9 +75,9 @@ ITEM_PIPELINES = {
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 10
+AUTOTHROTTLE_START_DELAY = 2 * 60
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY = 120
+AUTOTHROTTLE_MAX_DELAY = 5 * 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 # AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
