64e50944bd75c97a7b86ae93efcaf4adeae967fb
ezirmusitua <jferroal@gmail.com>
2017-11-19 03:02:57 +0800

    Add server tests placeholder
    Took 1 hour 51 minutes

---
+++ server/tests/tests.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import sys
+
+sys.path.append('..')
+
+from unittest import TestCase, main
+
+
+class TestUtils(TestCase):
+    pass
+
+
+class TestModel(TestCase):
+    pass
+
+
+class TestRoute(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    main()
