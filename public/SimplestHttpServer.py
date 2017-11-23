2f659e39b755c31351373b0268935f09eb5f99ec
ezirmusitua <jferroal@gmail.com>
2017-11-23 18:30:46 +0800

    Working on SimplestServer, fckng blckd ...
    Took 1 hour 47 minutes

--- public/SimplestHttpServer.py
+++ public/SimplestHttpServer.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
+import functools
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-class SimplestResponse(object):
-    def __init__(self, command, path, headers, wfile):
+class SimplestRequest(object):
+    def __init__(self, command, path, headers, rfile):
         self.__command = command
         self.__path = path
         self.__headers = headers
-        self.__wfile = wfile
-
-
-class SimplestRequest(object):
-    def __init__(self, request, make_response, rfile):
-        self.__request = request
-        self.__make_response = make_response
         self.__rfile = rfile
 
 
+class SimplestResponse(object):
+    def __init__(self, request, make_response, wfile):
+        self.__request = request
+        self.__make_response = make_response
+        self.__wfile = wfile
+
+
 class SimplestHandler(BaseHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.__get_routes = list()
-        self.__post_routes = list()
+    __get_routes = dict()
+    __post_routes = dict()
 
     def make_response(self, status, response_headers):
         # headers
@@ -33,24 +32,46 @@ class SimplestHandler(BaseHTTPRequestHandler):
         res = SimplestResponse(self.request, self.make_response, self.rfile)
         return req, res
 
-    def route(self, method, path):
+    def route(self, method, path, handler):
         # decorator to wrap func as route handler
         # parse path, and set in get/post_routes
-        pass
+        if method == 'GET':
+            SimplestHandler.__get_routes[path] = handler
+        if method == 'POST':
+            SimplestHandler.__post_routes[path] = handler
+
+    def get(self, route_handler, path):
+        @functools.wraps(route_handler)
+        def wrapped():
+            self.route('GET', path, route_handler)
+
+        return wrapped
+
+    def post(self, path, route_handler):
+        @functools.wraps(route_handler)
+        def wrapped():
+            self.route('POST', path, route_handler)
+
+        return wrapped
 
     def do_GET(self):
         req, res = self.create_req_res()
-        # dispatch to get_routes
-        pass
+        self.__get_routes[self.path](req, res)
 
     def do_POST(self):
         req, res = self.create_req_res()
-        # dispatch to get_routes
-        pass
+        self.__post_routes[self.path](req, res)
 
 
-def run(server_class=HTTPServer, handler_class=SimplestHandler, port=80):
+# 细细一想,事情并没有那么简单,,,
+# 1. 看 HTTPServer 的源码,了解调用 Handler 过程
+# 2. 外部实现,将 __routes 作为类变量,要保证单例
+
+def run(server_class=HTTPServer, handler_class=SimplestHandler, port=81):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print('\nStarting httpd...')
     httpd.serve_forever()
+
+
+run()
