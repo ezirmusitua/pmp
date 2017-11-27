17b5109c6f8c89e44f2939179db384dcf42abb96
ezirmusitua <jferroal@gmail.com>
2017-11-27 16:59:14 +0800

    Add validation task control API
    Use global variable to store validating process
    
    Took 3 hours 58 minutes

--- validator/serve.py
+++ validator/serve.py
@@ -9,13 +9,20 @@ from proxy_validator.proxy import ProxyModel
 from proxy_validator.database import Database
 from proxy_validator.validation import validation_chain
 
+global VALIDATION_TASK
+VALIDATION_TASK = None
+
 
 def get_stats(_req, res, _server):
     database = Database()
     database.connect()
     res.set_status(HTTPStatus.OK).set_headers({'Content-Type': 'application/json'}).end_headers()
-    total_count = database.count()
-    res.send('{"total_usable_count": %s}' % str(total_count)).end_send()
+    global VALIDATION_TASK
+    result_json = {
+        'is_validating': bool(VALIDATION_TASK and VALIDATION_TASK.is_alive()),
+        'total_usable_count': database.count()
+    }
+    res.send(json.dumps(result_json)).end_send()
 
 
 def query_proxies(req, res, _server):
@@ -40,25 +47,36 @@ def _run_proxies_validation(_server):
     db = Database()
     db.connect()
     for p in db.list({}):
-        print('validating %s' % p)
         t = Task(ProxyModel(p))
         validation_chain.start_handling(t)
 
 
 def start_validation(_req, res, _server):
-    print(_server.env)
     from multiprocessing import Process
-    p = Process(target=_run_proxies_validation, args=(_server,))
-    p.start()
-    # do not add join, otherwise it will block
-    res.set_status(HTTPStatus.OK).set_header('Content-Type', 'text/html').end_headers()
-    res.send('<html><body><p>Start validation task</p></body></html>').end_send()
+    res.set_status(HTTPStatus.OK).set_header('Content-Type', 'application/json').end_headers()
+    global VALIDATION_TASK
+    if not VALIDATION_TASK or not VALIDATION_TASK.is_alive():
+        VALIDATION_TASK = Process(target=_run_proxies_validation, args=(_server,))
+        VALIDATION_TASK.start()
+        # do not add join, otherwise it will block
+        res.send('{"status": "success"}').end_send()
+    else:
+        res.send('{"status": "failed"}').end_send()
 
 
-if __name__ == '__main__':
-    global_stats = {'is_running': False}
-    server = SimplestHttpServer()
-    server.route('GET', '/', get_stats)
-    server.route('GET', '/proxies', query_proxies)
-    server.route('POST', '/validation_task', start_validation)
-    server.run()
+def terminate_validation(_req, res, _server):
+    res.set_status(HTTPStatus.OK).set_header('Content-Type', 'application/json').end_headers()
+    global VALIDATION_TASK
+    if not VALIDATION_TASK or not VALIDATION_TASK.is_alive():
+        res.send('{"status": "failed"}').end_send()
+    else:
+        VALIDATION_TASK.terminate()
+        res.send('{"status": "success"}').end_send()
+
+
+server = SimplestHttpServer()
+server.route('GET', '/', get_stats)
+server.route('GET', '/proxies', query_proxies)
+server.route('POST', '/validation_task', start_validation)
+server.route('DELETE', '/validation_task', terminate_validation)
+server.run()
