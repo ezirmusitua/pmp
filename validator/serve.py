3a117811750fcf8399edbfa43199cc6e6391e7fd
ezirmusitua <jferroal@gmail.com>
2017-11-28 22:56:49 +0800

    Remove unused validator server routes
    Took 10 hours 57 minutes

--- validator/serve.py
+++ validator/serve.py
@@ -7,31 +7,7 @@ from public import SimplestHttpServer, HTTPStatus
 from proxy_validator.proxy import ProxyModel
 from proxy_validator.database import Database
 
-# from multiprocessing import Process
-# from proxy_validator.chain import Task
-# from proxy_validator.validation import validation_chain
-
-# global USER_VALIDATION_TASK
-# USER_VALIDATION_TASK = None
-
 server = SimplestHttpServer()
-
-
-def is_validation_running():
-    # global USER_VALIDATION_TASK
-    # return bool(USER_VALIDATION_TASK and USER_VALIDATION_TASK.is_alive())
-    return True
-
-
-# def run_validation_task():
-#     if is_validation_running():
-#         print('\n    Validation has started. ')
-#         return False
-#     global USER_VALIDATION_TASK
-#     USER_VALIDATION_TASK = Process(target=_do_proxies_validation)
-#     USER_VALIDATION_TASK.start()
-#     print('\n    Validation started. ')
-#     return True
 
 
 def get_stats(_req, res, _server):
@@ -39,7 +15,6 @@ def get_stats(_req, res, _server):
     database.connect()
     res.set_status(HTTPStatus.OK).set_headers({'Content-Type': 'application/json'}).end_headers()
     result_json = {
-        'is_validating': is_validation_running(),
         'total_usable_count': database.count()
     }
     res.send(json.dumps(result_json)).end_send()
@@ -63,33 +38,6 @@ def query_proxies(req, res, _server):
     res.send(return_str).end_send()
 
 
-# def _do_proxies_validation():
-#     db = Database()
-#     db.connect()
-#     for p in db.list({}):
-#         t = Task(ProxyModel(p))
-#         validation_chain.start_handling(t)
-
-
-# def start_validation(_req, res, _server):
-#     res.set_status(HTTPStatus.OK).set_header('Content-Type', 'application/json').end_headers()
-#     is_running = run_validation_task()
-#     res.send('{"status": "%s"}' % 'success' if is_running else 'failed').end_send()
-
-
-# def terminate_validation(_req, res, _server):
-#     # can only terminate user validation task
-#     res.set_status(HTTPStatus.OK).set_header('Content-Type', 'application/json').end_headers()
-#     global USER_VALIDATION_TASK
-#     if not USER_VALIDATION_TASK or not USER_VALIDATION_TASK.is_alive():
-#         res.send('{"status": "failed"}').end_send()
-#     else:
-#         USER_VALIDATION_TASK.terminate()
-#         res.send('{"status": "success"}').end_send()
-
-
 server.route('GET', '/', get_stats)
 server.route('GET', '/proxies', query_proxies)
-# server.route('POST', '/validation_task', start_validation)
-# server.route('DELETE', '/validation_task', terminate_validation)
 server.run()
