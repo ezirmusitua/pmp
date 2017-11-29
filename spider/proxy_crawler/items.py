fe36c550be5a9ae561a6deda23abde0e6404b4f5
ezirmusitua <jferroal@gmail.com>
2017-11-29 10:41:47 +0800

    Fix item loader proxy_type input processor
    Took 11 hours 27 minutes

--- spider/proxy_crawler/items.py
+++ spider/proxy_crawler/items.py
@@ -17,29 +17,6 @@ class StrCleaner(object):
         return list(map(lambda x: str.strip(x), values))
 
 
-class TimeStrConverter(object):
-    def __init__(self, format='%Y-%m-%d %H:%M:%S'):
-        self.format = format
-
-    def __call__(self, values):
-        def strptime(ts):
-            tt = datetime.datetime.strptime(ts, self.format).timetuple()
-            if tt.tm_year < 2017:
-                tmp = list(tt)
-                tmp[0] = 2017
-                tt = time.struct_time(tmp)
-            return tt
-
-        def to_timestamp(ts): return time.mktime(strptime(ts))
-
-        return list(map(to_timestamp, values))
-
-
-class StrToIntConverter(object):
-    def __call__(self, values):
-        return list(map(lambda x: int(x), values))
-
-
 class Proxy(scrapy.Item):
     ip_address = scrapy.Field()
     port = scrapy.Field()
@@ -49,5 +26,5 @@ class Proxy(scrapy.Item):
 class ProxyItemLoader(ItemLoader):
     default_input_processor = StrCleaner()
     default_output_processor = TakeFirst()
-    proxy_type_in = Identity()
+    proxy_type_in = StrCleaner()
     proxy_type_out = Identity()
