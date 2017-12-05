16566c6e32ef6f86db1c63bc0d1ff704a2476261
ezirmusitua <jferroal@gmail.com>
2017-12-05 09:57:46 +0800

    Add validator dockerfile and fix compose
    Took 1 hour 11 minutes

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
-mkdir pmp_deploy
-cp docker-compose.yml pmp_deploy
-# deploy server
+sudo rm -rf pmp
+mkdir pmp
+cp docker-compose.yml pmp/
+# server
 # create server_deploy
-mkdir pmp_deploy/server_deploy
+mkdir pmp/server_deploy
 # copy server source to server_deploy
-cp -r server/ pmp_deploy/server_deploy/server
+cp -r server/ pmp/server_deploy/server
 # copy public to server_deploy
-cp -r public/ pmp_deploy/server_deploy/public
-mv pmp_deploy/server_deploy/server/requirements.txt pmp_deploy/server_deploy/
-mv pmp_deploy/server_deploy/server/Dockerfile pmp_deploy/server_deploy/
-# run docker buid
-#sudo docker build .
-# run docker run
+cp -r public/ pmp/server_deploy/public
+mv pmp/server_deploy/server/requirements.txt pmp/server_deploy/
+mv pmp/server_deploy/server/Dockerfile pmp/server_deploy/
+
+# validator
+# create server_deploy
+mkdir pmp/validator_deploy
+# copy server source to server_deploy
+cp -r validator/ pmp/validator_deploy/validator
+# copy public to server_deploy
+cp -r public/ pmp/validator_deploy/public
+mv pmp/validator_deploy/validator/requirements.txt pmp/validator_deploy/
+mv pmp/validator_deploy/validator/Dockerfile pmp/validator_deploy/
